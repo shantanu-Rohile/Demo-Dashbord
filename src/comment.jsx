d927c69b19bb688cba17669d6b179770998fa8c1
Shantanu Rohile <ca22075shantanurohile@gmail.com>
2025-01-15 14:12:05 +0530

    DashBoard

---
+++ src/comment.jsx
@@ -0,0 +1,32 @@
+import React, { useState } from 'react'
+import Form from './form'
+import './Comments.css'
+
+export default function Comment() {
+    let[comments,setComment]=useState([{
+        fullName:"Rohan",
+        taskName:"Cleaning",
+        datandtime:""
+    }])
+
+    let addNewComments=(comment)=>{
+         setComment((currData)=>[...currData,comment])
+         console.log("Added new Comments")
+    }
+  return (
+    <div>
+       <Form addNewComments={addNewComments}/>
+       <hr />
+       {comments.map((comment, idx) => (
+        <div className="comment" key={idx}>
+          <span><strong>Name:</strong> {comment.fullName}</span>
+          <br />
+          <span><strong>Task:</strong> {comment.taskName}</span>
+          <br />
+          <span><strong>Date & Time:</strong> {comment.datandtime}</span>
+          <br />
+        </div>
+      ))}
+    </div>
+  )
+}
---
+++ src/form.jsx
@@ -0,0 +1,53 @@
+import React, { useState } from 'react'
+function Form({addNewComments}) {
+  let [formData, setFormData]=useState({
+    fullName:"",
+    taskName:"",
+    datandtime:""
+  });
+
+  let onHandleChange=((event)=>{
+    let name=event.target.name;
+    let value=event.target.value;
+    setFormData(
+      (currData)=>{
+      return{
+        ...currData,
+        [name]:value
+      }
+      }
+    )
+  })
+
+  let onChangeSubmit=(()=>{
+    console.log("Button is Clicked")
+    console.log(formData)
+    addNewComments(formData)
+    setFormData({
+      fullName:"",
+      taskName:"",
+      datandtime:""
+    })
+  })
+  return (
+    <div>
+       <h1>Admin Page</h1>
+     <label htmlFor="name">Name </label>
+     <input type="text" name="fullName" id="name" placeholder='Name of the devotee' value={formData.fullName} onChange={onHandleChange}/>
+     <br />
+     <br />
+     <label htmlFor="task">Task </label>
+     <input type="text" name="taskName" id="task" placeholder='Task' value={formData.taskName}  onChange={onHandleChange}/>
+     <br />
+     <br />
+     <label htmlFor="date">Date and Time </label>
+     <input type="datetime-local" name="datandtime" id="date" placeholder='Name of the devotee' value={formData.datandtime}  onChange={onHandleChange}/>
+     <br />
+     <br />
+     <button type='submit' onClick={onChangeSubmit}>Submit</button>
+    </div>
+
+  )
+}
+
+export default Form;
